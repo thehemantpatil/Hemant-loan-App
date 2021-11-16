fa50719dada22829ce8672fb404f7eca330ba1cf
Hemant Patil <73818024+thehemantpatil@users.noreply.github.com>
2021-11-16 15:20:42 +0530

    Delete Loan-App directory

--- Loan-App/src/main/java/com/example/demo/modal/UserInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.modal;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserInterface extends JpaRepository<User, Long>{
-		User findByEmail(String email);
-}
